4200ec0a8a42352ef3c3547e50dcd8a31ca45318
Ahmadibnumuthohhari4124023 <tibnu4823@gmail.com>
2026-06-19 18:38:20 +0700

    Update debug route dengan hash

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,8 @@
 Route::get('/debug-env', function () {
     $pw = env('DB_PASSWORD') ?? '';
     return response()->json([
-        'db_connection' => env('DB_CONNECTION'),
-        'db_host' => env('DB_HOST'),
-        'db_username' => env('DB_USERNAME'),
-        'db_password_raw_length' => strlen($pw),
-        'db_password_trimmed_length' => strlen(trim($pw)),
-        'db_password_first_15' => substr($pw, 0, 15),
-        'db_password_last_10' => substr($pw, -10),
-        'has_trailing_whitespace' => $pw !== rtrim($pw),
-        'has_leading_whitespace' => $pw !== ltrim($pw),
-        'has_quotes' => str_contains($pw, '"') || str_contains($pw, "'"),
+        'db_password_sha256' => hash('sha256', $pw),
+        'db_password_length' => strlen($pw),
     ]);
 });
 // Tambahkan baris-baris berikut ke dalam routes/web.php milikmu,
