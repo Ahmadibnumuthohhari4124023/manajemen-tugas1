363bb5ad3deb482906d73568b75df7538e9c432a
Ahmadibnumuthohhari4124023 <tibnu4823@gmail.com>
2026-06-19 17:48:38 +0700

    Tambah debug route sementara

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
-
+Route::get('/debug-env', function () {
+    return response()->json([
+        'db_connection' => env('DB_CONNECTION'),
+        'db_host' => env('DB_HOST'),
+        'db_username' => env('DB_USERNAME'),
+        'db_password_length' => strlen(env('DB_PASSWORD') ?? ''),
+        'db_password_preview' => substr(env('DB_PASSWORD') ?? '', 0, 20) . '...',
+    ]);
+});
 // Tambahkan baris-baris berikut ke dalam routes/web.php milikmu,
 // di bawah use Illuminate\Support\Facades\Route; yang sudah ada.
 
